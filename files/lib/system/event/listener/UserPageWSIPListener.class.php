cc254503830097730f366b5881f64a58524ea267
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2011-12-27 12:57:09 +0100

    Fixed #1517

--- files/lib/system/event/listener/UserPageWSIPListener.class.php
+++ files/lib/system/event/listener/UserPageWSIPListener.class.php
@@ -20,71 +20,78 @@ class UserPageWSIPListener implements EventListener {
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-		$user = $eventObj->frame->getUser();
-		
-		// news
-		if (MODULE_NEWS && PROFILE_SHOW_LAST_NEWS_ENTRIES) {
-			require_once(WSIP_DIR.'lib/data/news/ViewableNewsEntry.class.php');
-			$categoryIDArray = Category::getAccessibleCategoryIDArray(array('canViewCategory', 'canEnterCategory', 'canReadNewsEntry'));
+		if ($eventName == 'init') {
+			$eventObj->sqlSelects .= 'wsip_user.newsEntries,';
+			$eventObj->sqlJoins .= ' LEFT JOIN wsip'.WSIP_N.'_user wsip_user
+						ON (wsip_user.userID = user.userID) ';
+		}
+		else if ($eventName == 'assignVariables') {
+			$user = $eventObj->frame->getUser();
 			
-			if (count($categoryIDArray)) {
-				$entries = array();
-				$sql = "SELECT		entryID, subject, time
-					FROM		wsip".WSIP_N."_news_entry
-					WHERE		userID = ".$user->userID."
-							AND isDeleted = 0
-							AND isDisabled = 0
-							AND categoryID IN (".implode(',', $categoryIDArray).")
-							".(count(WCF::getSession()->getVisibleLanguageIDArray()) ? "AND languageID IN (".implode(',', WCF::getSession()->getVisibleLanguageIDArray()).")" : "")."
-					ORDER BY	time DESC";
-				$result = WCF::getDB()->sendQuery($sql, 5);
-				while ($row = WCF::getDB()->fetchArray($result)) {
-					$entries[] = new ViewableNewsEntry(null, $row);
-				}
+			// news
+			if (MODULE_NEWS && PROFILE_SHOW_LAST_NEWS_ENTRIES) {
+				require_once(WSIP_DIR.'lib/data/news/ViewableNewsEntry.class.php');
+				$categoryIDArray = Category::getAccessibleCategoryIDArray(array('canViewCategory', 'canEnterCategory', 'canReadNewsEntry'));
 				
-				if (count($entries)) {
-					WCF::getTPL()->assign(array(
-						'entries' => $entries,
-						'user' => $user
-					));
-					WCF::getTPL()->append('additionalContent2', WCF::getTPL()->fetch('userProfileLastNewsEntries'));
+				if (count($categoryIDArray)) {
+					$entries = array();
+					$sql = "SELECT		entryID, subject, time
+						FROM		wsip".WSIP_N."_news_entry
+						WHERE		userID = ".$user->userID."
+								AND isDeleted = 0
+								AND isDisabled = 0
+								AND categoryID IN (".implode(',', $categoryIDArray).")
+								".(count(WCF::getSession()->getVisibleLanguageIDArray()) ? "AND languageID IN (".implode(',', WCF::getSession()->getVisibleLanguageIDArray()).")" : "")."
+						ORDER BY	time DESC";
+					$result = WCF::getDB()->sendQuery($sql, 5);
+					while ($row = WCF::getDB()->fetchArray($result)) {
+						$entries[] = new ViewableNewsEntry(null, $row);
+					}
+					
+					if (count($entries)) {
+						WCF::getTPL()->assign(array(
+							'entries' => $entries,
+							'user' => $user
+						));
+						WCF::getTPL()->append('additionalContent2', WCF::getTPL()->fetch('userProfileLastNewsEntries'));
+					}
 				}
 			}
-		}
-		
-		// articles
-		if (MODULE_ARTICLE && PROFILE_SHOW_LAST_ARTICLES) {
-			require_once(WSIP_DIR.'lib/data/article/Article.class.php');
-			$categoryIDArray = Category::getAccessibleCategoryIDArray(array('canViewCategory', 'canEnterCategory', 'canReadArticle'));
 			
-			if (count($categoryIDArray)) {
-				// get number of articles
-				$sql = "SELECT	COUNT(*) AS count
-					FROM	wsip".WSIP_N."_article
-					WHERE	userID = ".$user->userID;
-				$row = WCF::getDB()->getFirstRow($sql);
-				$numberOfArticles = $row['count'];
+			// articles
+			if (MODULE_ARTICLE && PROFILE_SHOW_LAST_ARTICLES) {
+				require_once(WSIP_DIR.'lib/data/article/Article.class.php');
+				$categoryIDArray = Category::getAccessibleCategoryIDArray(array('canViewCategory', 'canEnterCategory', 'canReadArticle'));
 				
-				// get articles
-				$articles = array();
-				$sql = "SELECT		firstSectionID, subject, time
-					FROM		wsip".WSIP_N."_article
-					WHERE		userID = ".$user->userID."
-							AND categoryID IN (".implode(',', $categoryIDArray).")
-							".(count(WCF::getSession()->getVisibleLanguageIDArray()) ? "AND languageID IN (".implode(',', WCF::getSession()->getVisibleLanguageIDArray()).")" : "")."
-					ORDER BY	time DESC";
-				$result = WCF::getDB()->sendQuery($sql, 5);
-				while ($row = WCF::getDB()->fetchArray($result)) {
-					$articles[] = new Article(null, $row);
-				}
-				
-				if (count($articles)) {
-					WCF::getTPL()->assign(array(
-						'articles' => $articles,
-						'user' => $user,
-						'numberOfArticles' => $numberOfArticles
-					));
-					WCF::getTPL()->append('additionalContent2', WCF::getTPL()->fetch('userProfileLastArticles'));
+				if (count($categoryIDArray)) {
+					// get number of articles
+					$sql = "SELECT	COUNT(*) AS count
+						FROM	wsip".WSIP_N."_article
+						WHERE	userID = ".$user->userID;
+					$row = WCF::getDB()->getFirstRow($sql);
+					$numberOfArticles = $row['count'];
+					
+					// get articles
+					$articles = array();
+					$sql = "SELECT		firstSectionID, subject, time
+						FROM		wsip".WSIP_N."_article
+						WHERE		userID = ".$user->userID."
+								AND categoryID IN (".implode(',', $categoryIDArray).")
+								".(count(WCF::getSession()->getVisibleLanguageIDArray()) ? "AND languageID IN (".implode(',', WCF::getSession()->getVisibleLanguageIDArray()).")" : "")."
+						ORDER BY	time DESC";
+					$result = WCF::getDB()->sendQuery($sql, 5);
+					while ($row = WCF::getDB()->fetchArray($result)) {
+						$articles[] = new Article(null, $row);
+					}
+					
+					if (count($articles)) {
+						WCF::getTPL()->assign(array(
+							'articles' => $articles,
+							'user' => $user,
+							'numberOfArticles' => $numberOfArticles
+						));
+						WCF::getTPL()->append('additionalContent2', WCF::getTPL()->fetch('userProfileLastArticles'));
+					}
 				}
 			}
 		}
