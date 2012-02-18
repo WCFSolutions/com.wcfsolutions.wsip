59eb9f18b01cc1625b9c690b1fe5252ebf54fb71
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2012-02-18 16:00:10 +0100

    Fixed comment

--- files/lib/acp/form/ContentItemAddForm.class.php
+++ files/lib/acp/form/ContentItemAddForm.class.php
@@ -12,7 +12,7 @@ require_once(WCF_DIR.'lib/system/style/StyleManager.class.php');
  * Shows the content item add form.
  * 
  * @author	Sebastian Oettl
- * @copyright	2009-2011 WCF Solutions <http://www.wcfsolutions.com/index.html>
+ * @copyright	2009-2012 WCF Solutions <http://www.wcfsolutions.com/index.html>
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.wcfsolutions.wsip
  * @subpackage	acp.form
--- files/lib/data/page/location/ArticleSectionLocation.class.php
+++ files/lib/data/page/location/ArticleSectionLocation.class.php
@@ -9,7 +9,7 @@ require_once(WCF_DIR.'lib/data/page/location/Location.class.php');
  * ArticleSectionLocation is an implementation of Location for the article section page.
  * 
  * @author	Sebastian Oettl
- * @copyright	2009-2011 WCF Solutions <http://www.wcfsolutions.com/index.html>
+ * @copyright	2009-2012 WCF Solutions <http://www.wcfsolutions.com/index.html>
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.wcfsolutions.wsip
  * @subpackage	data.page.location
